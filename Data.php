2f143f1f0032cded11efcce1ef85beb8ba65370d
Nofrets Poai <opetstudio@gmail.com>
2019-08-15 13:14:06 +0700

    dont track daata.php

--- Data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DatabaseType = 'mysqli'; 
-$DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'manj4785_opensis'; 
-$DatabasePassword = 'Opensis123!'; 
-$DatabaseName = 'manj4785_opensis_db'; 
-$DatabasePort = '3306'; 
-?>
