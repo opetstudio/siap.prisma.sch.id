f7d4e1aae7fa5fbf6b1feb772905652796a149d4
Nofrets Poai <opetstudio@gmail.com>
2019-08-15 12:10:43 +0700

    init

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'manj4785_opensis'; 
+$DatabasePassword = 'Opensis123!'; 
+$DatabaseName = 'manj4785_opensis_db'; 
+$DatabasePort = '3306'; 
+?>
